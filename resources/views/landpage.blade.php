951826ec7674de7789223e0517529e1b8b97b395
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2025-07-24 22:36:37 +0700

    commit

---
+++ resources/views/landpage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     @vite('resources/css/app.css')
+    <title>Dapurku</title>
+</head>
+<body>
+    <nav class="bg-gray-400 text-white p-4">
+        <div class="container mx-auto flex justify-between items-center h-12">
+            <h1 class="font-bold m-8px p-12px">Dapurku</h1>
+            <ul class="flex flex-row space-x-6 font-[<Poppins>]">
+                <li class="hover:text-blue-500"><a href="/">Beranda</a></li>
+                <li class="hover:text-blue-500"><a href="/recipes">Tentang Kami</a></li>
+                <li class="hover:text-blue-500"><a href="/about">Menu</a></li>
+                <li class="hover:text-blue-500"><a href="/contact">Feedback</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
